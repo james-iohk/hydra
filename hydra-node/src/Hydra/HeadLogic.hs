a7e48d2f71d2bde63b708f0a274f1c4f5f3fa4a9
Arnaud Bailly <arnaud.bailly@iohk.io>
2023-07-08 18:21:59 +0200

    Extract constituents from HeadLogic into own modules
    Timing with split modules:
    
    ```
    cabal build hydra-node:lib:hydra-node  26.85s user 1.92s system 127% cpu 22.599 total
    cabal build hydra-node:lib:hydra-node  27.73s user 1.83s system 126% cpu 23.337 total
    cabal build hydra-node:lib:hydra-node  26.71s user 1.95s system 128% cpu 22.350 total
    ```
    
    Timing with single HeadLogic module (3 runs):
    ```
    cabal build hydra-node:lib:hydra-node  28.23s user 1.84s system 121% cpu 24.753 total
    cabal build hydra-node:lib:hydra-node  27.37s user 1.82s system 120% cpu 24.186 total
    cabal build hydra-node:lib:hydra-node  27.34s user 1.84s system 121% cpu 24.041 total
    ```

--- hydra-node/src/Hydra/HeadLogic.hs
+++ hydra-node/src/Hydra/HeadLogic.hs
@@ -13,11 +13,19 @@
 --     * A third part detailing the /On-Chain Verification (OCV)/ protocol, i.e. the abstract "smart contracts" that are need to provide on-chain security.
 --
 -- This module is about the first two parts, while the "Hydra.Contract.Head" module in 'hydra-plutus' covers the third part.
-module Hydra.HeadLogic where
+module Hydra.HeadLogic (
+  update,
+  defaultTTL,
+  Environment (..),
+  module Hydra.HeadLogic.Event,
+  module Hydra.HeadLogic.Error,
+  module Hydra.HeadLogic.State,
+  module Hydra.HeadLogic.Outcome,
+  module Hydra.HeadLogic.Snapshot,
+) where
 
 import Hydra.Prelude
 
-import Data.List (elemIndex)
 import qualified Data.Map.Strict as Map
 import Data.Set ((\\))
 import qualified Data.Set as Set
@@ -32,26 +40,51 @@ import Hydra.Chain (
   IsChainState (chainStateSlot),
   OnChainTx (..),
   PostChainTx (..),
-  PostTxError,
  )
-import Hydra.ContestationPeriod
+import Hydra.ContestationPeriod (ContestationPeriod)
 import Hydra.Crypto (
-  HydraKey,
   Signature,
-  SigningKey,
-  VerificationKey,
   Verified (..),
   aggregateInOrder,
   sign,
   verifyMultiSignature,
  )
+import Hydra.HeadLogic.Error (
+  LogicError (..),
+  RequirementFailure (..),
+ )
+import Hydra.HeadLogic.Event (
+  Event (..),
+  TTL,
+ )
+import Hydra.HeadLogic.Outcome (
+  Effect (..),
+  Outcome (..),
+  WaitReason (..),
+  collectEffects,
+  collectWaits,
+ )
+import Hydra.HeadLogic.Snapshot (emitSnapshot, isLeader)
+import Hydra.HeadLogic.State (
+  ClosedState (..),
+  Committed,
+  CoordinatedHeadState (..),
+  Environment (..),
+  HeadState (..),
+  IdleState (IdleState, chainState),
+  InitialState (..),
+  OpenState (..),
+  PendingCommits,
+  SeenSnapshot (..),
+  getChainState,
+  seenSnapshotNumber,
+  setChainState,
+ )
 import Hydra.Ledger (
-  ChainSlot,
   IsTx,
   Ledger (..),
   TxIdType,
   UTxOType,
-  ValidationError,
   applyTransactions,
   txId,
  )
@@ -61,270 +94,7 @@ import Hydra.Snapshot (ConfirmedSnapshot (..), Snapshot (..), SnapshotNumber, ge
 
 -- * Types
 
--- TODO: Move logic up and types down or re-organize using explicit exports
-
--- | The different events which are processed by the head logic (the "core").
--- Corresponding to each of the "shell" layers, we distinguish between events
--- from the client, the network and the chain.
-data Event tx
-  = -- | Event received from clients via the "Hydra.API".
-    ClientEvent {clientInput :: ClientInput tx}
-  | -- | Event received from peers via a "Hydra.Network".
-    --
-    --  * `ttl` is a simple counter that's decreased every time the event is
-    --    reenqueued due to a wait. It's default value is `defaultTTL`
-    NetworkEvent {ttl :: TTL, party :: Party, message :: Message tx}
-  | -- | Event received from the chain via a "Hydra.Chain".
-    OnChainEvent {chainEvent :: ChainEvent tx}
-  | -- | Event to re-ingest errors from 'postTx' for further processing.
-    PostTxError {postChainTx :: PostChainTx tx, postTxError :: PostTxError tx}
-  deriving stock (Generic)
-
-deriving instance (IsChainState tx) => Eq (Event tx)
-deriving instance (IsChainState tx) => Show (Event tx)
-deriving instance (IsChainState tx) => ToJSON (Event tx)
-deriving instance (IsChainState tx) => FromJSON (Event tx)
-
-instance
-  ( IsTx tx
-  , Arbitrary (ChainStateType tx)
-  ) =>
-  Arbitrary (Event tx)
-  where
-  arbitrary = genericArbitrary
-
--- | Analogous to events, the pure head logic "core" can have effects emited to
--- the "shell" layers and we distinguish the same: effects onto the client, the
--- network and the chain.
-data Effect tx
-  = -- | Effect to be handled by the "Hydra.API", results in sending this 'ServerOutput'.
-    ClientEffect {serverOutput :: ServerOutput tx}
-  | -- | Effect to be handled by a "Hydra.Network", results in a 'Hydra.Network.broadcast'.
-    NetworkEffect {message :: Message tx}
-  | -- | Effect to be handled by a "Hydra.Chain", results in a 'Hydra.Chain.postTx'.
-    OnChainEffect {postChainTx :: PostChainTx tx}
-  deriving stock (Generic)
-
-deriving instance (IsChainState tx) => Eq (Effect tx)
-deriving instance (IsChainState tx) => Show (Effect tx)
-deriving instance (IsChainState tx) => ToJSON (Effect tx)
-deriving instance (IsChainState tx) => FromJSON (Effect tx)
-
-instance
-  ( IsTx tx
-  , Arbitrary (ChainStateType tx)
-  ) =>
-  Arbitrary (Effect tx)
-  where
-  arbitrary = genericArbitrary
-
--- | The main state of the Hydra protocol state machine. It holds both, the
--- overall protocol state, but also the off-chain 'CoordinatedHeadState'.
---
--- Each of the sub-types (InitialState, OpenState, etc.) contain black-box
--- 'chainState' corresponding to 'OnChainEvent' that has been observed leading
--- to the state.
---
--- Note that rollbacks are currently not fully handled in the head logic and
--- only this internal chain state gets replaced with the "rolled back to"
--- version.
-data HeadState tx
-  = Idle (IdleState tx)
-  | Initial (InitialState tx)
-  | Open (OpenState tx)
-  | Closed (ClosedState tx)
-  deriving stock (Generic)
-
-instance (IsTx tx, Arbitrary (ChainStateType tx)) => Arbitrary (HeadState tx) where
-  arbitrary = genericArbitrary
-
-deriving instance (IsTx tx, Eq (ChainStateType tx)) => Eq (HeadState tx)
-deriving instance (IsTx tx, Show (ChainStateType tx)) => Show (HeadState tx)
-deriving instance (IsTx tx, ToJSON (ChainStateType tx)) => ToJSON (HeadState tx)
-deriving instance (IsTx tx, FromJSON (ChainStateType tx)) => FromJSON (HeadState tx)
-
--- | Get the chain state in any 'HeadState'.
-getChainState :: HeadState tx -> ChainStateType tx
-getChainState = \case
-  Idle IdleState{chainState} -> chainState
-  Initial InitialState{chainState} -> chainState
-  Open OpenState{chainState} -> chainState
-  Closed ClosedState{chainState} -> chainState
-
--- | Update the chain state in any 'HeadState'.
-setChainState :: ChainStateType tx -> HeadState tx -> HeadState tx
-setChainState chainState = \case
-  Idle st -> Idle st{chainState}
-  Initial st -> Initial st{chainState}
-  Open st -> Open st{chainState}
-  Closed st -> Closed st{chainState}
-
--- ** Idle
-
--- | An 'Idle' head only having a chain state with things seen on chain so far.
-newtype IdleState tx = IdleState {chainState :: ChainStateType tx}
-  deriving (Generic)
-
-deriving instance Eq (ChainStateType tx) => Eq (IdleState tx)
-deriving instance Show (ChainStateType tx) => Show (IdleState tx)
-deriving anyclass instance ToJSON (ChainStateType tx) => ToJSON (IdleState tx)
-deriving anyclass instance FromJSON (ChainStateType tx) => FromJSON (IdleState tx)
-
-instance (Arbitrary (ChainStateType tx)) => Arbitrary (IdleState tx) where
-  arbitrary = genericArbitrary
-
--- ** Initial
-
--- | An 'Initial' head which already has an identity and is collecting commits.
-data InitialState tx = InitialState
-  { parameters :: HeadParameters
-  , pendingCommits :: PendingCommits
-  , committed :: Committed tx
-  , chainState :: ChainStateType tx
-  , headId :: HeadId
-  }
-  deriving (Generic)
-
-deriving instance (IsTx tx, Eq (ChainStateType tx)) => Eq (InitialState tx)
-deriving instance (IsTx tx, Show (ChainStateType tx)) => Show (InitialState tx)
-deriving instance (IsTx tx, ToJSON (ChainStateType tx)) => ToJSON (InitialState tx)
-deriving instance (IsTx tx, FromJSON (ChainStateType tx)) => FromJSON (InitialState tx)
-
-instance (IsTx tx, Arbitrary (ChainStateType tx)) => Arbitrary (InitialState tx) where
-  arbitrary = do
-    InitialState
-      <$> arbitrary
-      <*> arbitrary
-      <*> arbitrary
-      <*> arbitrary
-      <*> arbitrary
-
-type PendingCommits = Set Party
-
-type Committed tx = Map Party (UTxOType tx)
-
--- ** Open
-
--- | An 'Open' head with a 'CoordinatedHeadState' tracking off-chain
--- transactions.
-data OpenState tx = OpenState
-  { parameters :: HeadParameters
-  , coordinatedHeadState :: CoordinatedHeadState tx
-  , chainState :: ChainStateType tx
-  , headId :: HeadId
-  , currentSlot :: ChainSlot
-  }
-  deriving (Generic)
-
-deriving instance (IsTx tx, Eq (ChainStateType tx)) => Eq (OpenState tx)
-deriving instance (IsTx tx, Show (ChainStateType tx)) => Show (OpenState tx)
-deriving instance (IsTx tx, ToJSON (ChainStateType tx)) => ToJSON (OpenState tx)
-deriving instance (IsTx tx, FromJSON (ChainStateType tx)) => FromJSON (OpenState tx)
-
-instance (IsTx tx, Arbitrary (ChainStateType tx)) => Arbitrary (OpenState tx) where
-  arbitrary =
-    OpenState
-      <$> arbitrary
-      <*> arbitrary
-      <*> arbitrary
-      <*> arbitrary
-      <*> arbitrary
-
--- | Off-chain state of the Coordinated Head protocol.
-data CoordinatedHeadState tx = CoordinatedHeadState
-  { seenUTxO :: UTxOType tx
-  -- ^ The latest UTxO resulting from applying 'seenTxs' to
-  -- 'confirmedSnapshot'. Spec: L̂
-  , seenTxs :: [tx]
-  -- ^ List of seen transactions pending inclusion in a snapshot. Spec: T̂
-  , allTxs :: Map.Map (TxIdType tx) tx
-  -- ^ Map containing all the transactions ever seen by this node and not yet included in a snapshot
-  , confirmedSnapshot :: ConfirmedSnapshot tx
-  -- ^ The latest confirmed snapshot. Spec: U̅, s̅ and σ̅
-  , seenSnapshot :: SeenSnapshot tx
-  -- ^ Last seen snapshot and signatures accumulator. Spec: Û, ŝ and Σ̂
-  }
-  deriving stock (Generic)
-
-deriving instance IsTx tx => Eq (CoordinatedHeadState tx)
-deriving instance IsTx tx => Show (CoordinatedHeadState tx)
-deriving instance IsTx tx => ToJSON (CoordinatedHeadState tx)
-deriving instance IsTx tx => FromJSON (CoordinatedHeadState tx)
-
-instance IsTx tx => Arbitrary (CoordinatedHeadState tx) where
-  arbitrary = genericArbitrary
-
--- | Data structure to help in tracking whether we have seen or requested a
--- ReqSn already and if seen, the signatures we collected already.
-data SeenSnapshot tx
-  = -- | Never saw a ReqSn.
-    NoSeenSnapshot
-  | -- | No snapshot in flight with last seen snapshot number as given.
-    LastSeenSnapshot {lastSeen :: SnapshotNumber}
-  | -- | ReqSn was sent out and it should be considered already in flight.
-    RequestedSnapshot
-      { lastSeen :: SnapshotNumber
-      , requested :: SnapshotNumber
-      }
-  | -- | ReqSn for given snapshot was received.
-    SeenSnapshot
-      { snapshot :: Snapshot tx
-      , signatories :: Map Party (Signature (Snapshot tx))
-      -- ^ Collected signatures and so far.
-      }
-  deriving stock (Generic)
-
-instance IsTx tx => Arbitrary (SeenSnapshot tx) where
-  arbitrary = genericArbitrary
-
-deriving instance IsTx tx => Eq (SeenSnapshot tx)
-deriving instance IsTx tx => Show (SeenSnapshot tx)
-deriving instance IsTx tx => ToJSON (SeenSnapshot tx)
-deriving instance IsTx tx => FromJSON (SeenSnapshot tx)
-
--- | Get the last seen snapshot number given a 'SeenSnapshot'.
-seenSnapshotNumber :: SeenSnapshot tx -> SnapshotNumber
-seenSnapshotNumber = \case
-  NoSeenSnapshot -> 0
-  LastSeenSnapshot{lastSeen} -> lastSeen
-  RequestedSnapshot{lastSeen} -> lastSeen
-  SeenSnapshot{snapshot = Snapshot{number}} -> number
-
--- ** Closed
-
--- | An 'Closed' head with an current candidate 'ConfirmedSnapshot', which may
--- be contested before the 'contestationDeadline'.
-data ClosedState tx = ClosedState
-  { parameters :: HeadParameters
-  , confirmedSnapshot :: ConfirmedSnapshot tx
-  , contestationDeadline :: UTCTime
-  , readyToFanoutSent :: Bool
-  -- ^ Tracks whether we have informed clients already about being
-  -- 'ReadyToFanout'.
-  , chainState :: ChainStateType tx
-  , headId :: HeadId
-  }
-  deriving (Generic)
-
-deriving instance (IsTx tx, Eq (ChainStateType tx)) => Eq (ClosedState tx)
-deriving instance (IsTx tx, Show (ChainStateType tx)) => Show (ClosedState tx)
-deriving instance (IsTx tx, ToJSON (ChainStateType tx)) => ToJSON (ClosedState tx)
-deriving instance (IsTx tx, FromJSON (ChainStateType tx)) => FromJSON (ClosedState tx)
-
-instance (IsTx tx, Arbitrary (ChainStateType tx)) => Arbitrary (ClosedState tx) where
-  arbitrary =
-    ClosedState
-      <$> arbitrary
-      <*> arbitrary
-      <*> arbitrary
-      <*> arbitrary
-      <*> arbitrary
-      <*> arbitrary
-
 -- ** Other types
-
-type TTL = Natural
-
 defaultTTL :: TTL
 defaultTTL = 5
 
@@ -1162,11 +932,3 @@ update env ledger st ev = case (st, ev) of
     Effects [ClientEffect $ CommandFailed clientInput]
   _ ->
     Error $ InvalidEvent ev st
-
--- * Snapshot helper functions
-
-isLeader :: HeadParameters -> Party -> SnapshotNumber -> Bool
-isLeader HeadParameters{parties} p sn =
-  case p `elemIndex` parties of
-    Just i -> ((fromIntegral sn - 1) `mod` length parties) == i
-    _ -> False
